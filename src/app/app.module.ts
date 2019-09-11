73db7312f5de4a6b52f4bf885cfd813f0af8ad79
Amruth Pillai <im.amruth@gmail.com>
2019-09-11 22:17:57 +0200

    create sidebar component and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SidebarComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
