f74f3a8a806488a8f615b141b672658232a4c2fc
Amruth Pillai <im.amruth@gmail.com>
2019-09-13 20:57:44 +0200

    add server for heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./dist/GeekyAnts-Assignment'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/GeekyAnts-Assignment/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
